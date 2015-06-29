5d65d98255f6229e4a5313021c4efec0f7d41759
simpletrain <zsy498761554@live.com>
2015-06-29 23:55:19 +0800

    Finish section-1/practice-2

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,3 +1,13 @@
 function collect_same_elements(collection_a, collection_b) {
-  //在这里写入代码
+  var result = [];
+  for(var i=0;i<collection_a.length;i++){
+    for(var x=0;x<collection_b.length;x++){
+      for(var y=0;y<collection_b[x].length;y++){
+        if(collection_a[i]===collection_b[x][y]){
+          result.push(collection_a[i]);
+        }
+      }
+    }
+  }
+  return result;
 }
