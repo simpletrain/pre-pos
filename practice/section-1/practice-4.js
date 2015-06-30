80e886a4ec106c20241924963756232f23363a2b
simpletrain <zsy498761554@live.com>
2015-06-30 17:19:53 +0800

    First complete sec1-practice3&4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,3 +1,11 @@
 function collect_same_elements(collection_a, object_b) {
-  //在这里写入代码
+  var result = [];
+  for(var i=0;i<collection_a.length;i++){
+    for(var x=0;x<object_b.value.length;x++){
+      if(collection_a[i].key===object_b.value[x]){
+        result.push(collection_a[i].key);
+      }
+    }
+  }
+  return result;
 }
