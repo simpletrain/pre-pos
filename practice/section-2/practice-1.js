11978ee33e065783117259537a27c1d20a59102d
simpletrain <zsy498761554@live.com>
2015-06-30 21:28:25 +0800

    First complete section2

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,3 +1,16 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var result = [];
+  var now_count = "";
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]!==now_count){
+      result[result.length]={};
+      result[result.length-1].key=collection[i];
+      result[result.length-1].count=1;
+      now_count = collection[i];
+    }
+    else{
+      result[result.length-1].count++;
+    }
+  }
+  return result;
 }
--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,3 +1,25 @@
 function count_same_elements(collection) {
-  //在这里写入代码
+  var result = [];
+  var now_count = "";
+  for(var i=0;i<collection.length;i++){
+    if(collection[i]!==now_count&&collection[i].length==1){
+      result[result.length]={};
+      result[result.length-1].key=collection[i];
+      result[result.length-1].count=1;
+      now_count = collection[i];
+    }
+    else if(collection[i].length==1){
+      result[result.length-1].count++;
+    }
+    else if(collection[i][0]===now_count){
+      result[result.length-1].count+=collection[i][2];
+    }
+    else{
+      result[result.length]={};
+      result[result.length-1].key=collection[i][0];
+      result[result.length-1].count=parseInt((collection[i][2]));
+      now_count = collection[i][0];
+    }
+  }
+  return result;
 }
