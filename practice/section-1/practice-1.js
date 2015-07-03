1e6791346f20e9771d30644786b18746d6038fdd
simpletrain <zsy498761554@live.com>
2015-07-03 14:09:43 +0800

    Reformat code

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -1,8 +1,8 @@
 function collect_same_elements(collection_a, collection_b) {
   var result = [];
-  for(var i=0;i<collection_a.length;i++){
-    for(var x=0;x<collection_b.length;x++){
-      if(collection_a[i]===collection_b[x]){
+  for (var i = 0; i < collection_a.length; i++) {
+    for (var x = 0; x < collection_b.length; x++) {
+      if (collection_a[i] === collection_b[x]) {
         result.push(collection_a[i]);
       }
     }
